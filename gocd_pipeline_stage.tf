bd6663fedc948f5fd610d5a02ca5aeefcdc5f8b4
Keshav-Rai001 <42931112+Keshav-Rai001@users.noreply.github.com>
2018-09-21 14:32:15 +0530

    Add files via upload

---
+++ gocd_pipeline_stage.tf
@@ -0,0 +1,39 @@
+
+
+
+provider "gocd" 
+{
+  baseurl="${var.baseurl}"
+  #skip_ssl_check=true
+}
+
+provider "github" 
+{
+  token        = "af2e97f1cf2208929e480c3b3a28881d5c002c3e"
+  #organization = "${var.github_organization}"
+}
+resource "gocd_pipeline_stage" "build" 
+{
+  name = "${var.stage_name}"
+  pipeline = "${var.pipeline_name}"
+  jobs =[<<JOB
+ {
+  "name": "plan",
+  "tasks": 
+  [
+  {
+    "type": "ant",
+    "attributes": 
+    {
+      "run_if": ["passed"],
+      "build_file": "https://af2e97f1cf2208929e480c3b3a28881d5c002c3e@github.com/Keshav-Rai001/terraform-with-gocd/blob/master/build.xml",
+      "target": "https://af2e97f1cf2208929e480c3b3a28881d5c002c3e@github.com/Keshav-Rai001/terraform-with-gocd/blob/master/helloWorld.java"
+      
+    }
+  }
+  ]
+ }
+JOB
+]
+}
+
---
+++ var3.tf
@@ -0,0 +1,11 @@
+variable "baseurl" {
+  description = "URL of GoCD"
+}
+
+variable "stage_name" {
+  description = "name of the stage."
+}
+
+variable "pipeline_name" {
+  description = "Name of GoCD-pipeline"
+}
